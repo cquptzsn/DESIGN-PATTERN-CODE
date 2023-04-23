fa6c87c43fb4d898f0ffed171f685db7d5c04673
zhoushengnan02 <zhoushengnan02@meituan.com>
2023-04-23 22:36:43 +0800

    feat: 使用generator遍历DOM

---
+++ webpack-for-ts/src/Iterator/generatorDOM.ts
@@ -0,0 +1,18 @@
+// generator + yield 遍历 DOM 树
+
+function* traverse(elemList: Element[]): any {
+    for (const elem of elemList) {
+        yield elem
+        const children = Array.from(elem.children)
+        if(children.length) {
+            yield* traverse(children)
+        }
+    }
+}
+
+const container = document.getElementById('container')
+if(container) {
+    for (let node of traverse([container])) {
+        console.log(node)
+    }
+}
--- webpack-for-ts/webpack.config.js
+++ webpack-for-ts/webpack.config.js
@@ -9,7 +9,7 @@ const tplPath = path.join(__dirname, 'tpl')
 module.exports = {
     mode: 'development',
     // entry: path.resolve(srcPath, 'index'),
-    entry: path.resolve(srcPath, 'generator.ts'), // 这里也要根据不同的文件进行更改
+    entry: path.resolve(srcPath, 'generatorDOM.ts'), // 这里也要根据不同的文件进行更改
     module: {
         rules: [
             {
